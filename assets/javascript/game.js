9755fe2e3f3337db4e799b21f9fb94aaca1526e1
C Perez <connorperez@gmail.com>
2019-03-30 09:48:36 -0500

    done, tweaked and updated, no bonuses yet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,14 +7,14 @@ var computerRandom = ""; // string in which gotName will be held
 var computerAndUser = []; // holds both the computer generated word and user guess
 var lettersInName = []; // holds each letter as individual item
 var underscores = 0; // amount of underscores for each word. will match lettersInName
-var wrongLetters = []; // puts all wrong letters to side for user
+var wrongLetters = []; // puts all wrong letters to side for user in empty array to start
 
 
 
 
 // First function, to be run when the first game starts and after a game is won or lost 
 
-function setup(){
+function setup() {
 
     // randomly generates name from gotNames
     computerRandom = gotNames[Math.floor(Math.random() * gotNames.length)];
@@ -33,80 +33,79 @@ function setup(){
     wrongLetters = [];
     computerAndUser = [];
 
-    // Loops through the length of word and pushes a _ for each letter
-    for (var i = 0; i < underscores; i++){
+    // Loops through the length of word and pushes a _ for each letter to guess
+    for (var i = 0; i < underscores; i++) {
         computerAndUser.push("_");
     }
     console.log(computerAndUser);
 
-    // Updates html on the screen, and combines the word that needs to be guessed so that user can interact with it
-    document.getElementById("computerWord").innerHTML = computerAndUser.join(" ");
-    document.getElementById("numGuesses").innerHTML = numberOfGuesses;
-    document.getElementById("winScore").innerHTML = wins;
-    document.getElementById("lossScore").innerHTML = losses;
-    
+
+    updateResults();
+
+}
+
+// Updates html on the screen, and combines the word that needs to be guessed so that user can interact with it
+function updateResults() {
+
+    // First adds the computer word to the HTML. second two are for wins/losses
+    document.getElementById("computerWord").innerText = computerAndUser.join(" ");
+    document.getElementById("winScore").innerText = wins;
+    document.getElementById("lossScore").innerText = losses;
 
 }
 
 // this checks if the game is won or lost
-function testWinOrLoss(){
+function testWinOrLoss() {
 
-    document.getElementById("numGuesses").innerHTML = numberOfGuesses;
-    document.getElementById("computerWord").innerHTML = computerAndUser.join(" ");
-    document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
+    // First updates numbers of guesses, second adds content to main word, third adds keys to wrong letters
+    document.getElementById("numGuesses").innerText = numberOfGuesses;
+    document.getElementById("computerWord").innerText = computerAndUser.join(" ");
+    document.getElementById("wrongGuesses").innerText = wrongLetters.join(" ");
 
 
     // compares the lettersInName (chosen computer word) to the full guess, if it is even then it is a win
-    if (lettersInName.toString() == computerAndUser.toString()){
+    if (lettersInName.toString() == computerAndUser.toString()) {
         wins++;
         console.log("testing if won");
 
-        // specifying where to make changes for wins (same for losses below)
-        document.getElementById("winScore").innerHTML = wins;
-
-        // if (computerRandom === "arya"){
-        //     document.getElementById("img").src = "https://imgix.bustle.com/rehost/2016/9/14/65fc1f3e-e14c-4d30-b3bd-82e849646826.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70";
-        
-        // }
-
+        // reset game
         setup();
 
 
-        
     }
 
+
+
     // same thing but for loss, and I used guesses = 0 to show loss. 
-    else if (numberOfGuesses == 0){
+    else if (numberOfGuesses == 0) {
         losses++;
         console.log("testing if lost");
-
-        document.getElementById("lossScore").innerHTML = losses;
 
         // reset up the game
         setup();
     }
 }
 
-function checkUserKeys(letter){
-    var isLetterInWord = false;
+function checkUserKeys(key) {
+    var isKeyInWord = false;
 
-    // runs through computer word, and checks if key pressed matches
-    for (var i = 0; i < underscores; i++){
-        if (computerRandom[i] == letter){
-            isLetterInWord = true;
+    // these two loops runs through computer word, and checks if key pressed matches
+    for (var i = 0; i < underscores; i++) {
+        if (computerRandom[i] == key) {
+            isKeyInWord = true;
         }
     }
 
-    if (isLetterInWord){
-    for ( var i = 0; i < underscores; i++){
-        if (computerRandom[i] == letter){
-            computerAndUser[i] = letter;
+    if (isKeyInWord) {
+        for (var i = 0; i < underscores; i++) {
+            if (computerRandom[i] == key) {
+                computerAndUser[i] = key;
+            }
         }
     }
-}
     // if it does not match, push the letter to wrongLetters group which is displayed on screen through GEBID and subtract number guesses
     else {
-        wrongLetters.push(letter);
+        wrongLetters.push(key);
         numberOfGuesses--;
     }
 
@@ -115,12 +114,13 @@ function checkUserKeys(letter){
 
 
 
+
 // Main process
 
 setup();
 
 // after game is setup, pressing key is what triggers the rest of the functions and analyzes/outputs results 
-document.onkeydown = function(event){
+document.onkeydown = function (event) {
 
     // found the below var = string function on a stack overflow example. 
     // https://stackoverflow.com/questions/38428160/javascript-replacing-characters-in-string-from-keycode-event
